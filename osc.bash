8eec30e319c985be6cdeb6967d739865df37e3e0
Gabe Schwartz <hapemask@gmail.com>
2026-01-01 17:15:57 -0800

    Pass env vars to osc.bash to allow for profiling with LINE_PROFILING=1.

--- osc.bash
+++ osc.bash
@@ -4,4 +4,4 @@
 # cpu3.  Run the python code on cpu2 s.t. we can set affinity 3 on the ADC
 # sampler thread and give it maximum priority.
 
-sudo taskset -c 2 python osc.py
+sudo -E taskset -c 2 python3 osc.py
